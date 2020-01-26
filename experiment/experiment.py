cec7789c939daee7390f89284575df06df85fc6f
Chris Arderne <chris@rdrn.me>
2020-01-26 16:41:17 +0100

    Refactor experiment and change config for next run

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,4 +1,3 @@
-from functools import reduce
 from os.path import dirname, join
 from random import sample
 
@@ -40,96 +39,58 @@ class Experiment(rv.ExperimentSet):
             "stride": 300,  # slide over 300px to generate each new chip
         }
 
-        # Training configuration: try to improve performance by tuning these
+        # Training config
         config = {
-            "batch_size": 8,  # 8 chips per batch
-            "num_epochs": 6,  # complete 6 epochs
+            "batch_size": 256,  # (originally 8)
+            "num_epochs": 12,  # make multiple of 4 (originally 6)
             "debug": True,  # produce example chips to help with debugging
-            "lr": 1e-4,  # set learning
+            "lr": 6e-3,  # set max lr (originally 1e-4)
             "one_cycle": True,  # use cyclic learning rate scheduler
             "model_arch": "resnet50",  # model architecture
             "loss_fn": "JaccardLoss",
             "augmentors": ["RandomSizedCrop"],
         }
 
+        # Use smaller subset and quicker options for test runs
         if test:
             train_ids = [("acc", "d41d81")]
             valid_ids = [("acc", "a42435")]
-
             config["batch_size"] = 2
             config["num_epochs"] = 1
-
-            # In this 'test' scenario, we will generate a small number of chips
             chip_opts = {"window_method": "random_sample", "chips_per_scene": 10}
-
-            # Test only 5 items
-            all_test_items = [next(all_test_items) for _ in range(5)]
-
-            # Modify the experiment ID
+            all_test_items = [next(all_test_items) for _ in range(50)]
             experiment_id += "-TEST"
 
-        def make_train_scenes(item):
-            area = item.get_parent().id
-            label_uri = join(
-                dirname(train_stac_uri), area, f"{item.id}-labels", f"{item.id}.geojson"
-            )
-
-            i = 0
-            images_remaining = True
-            scenes = []
-            while images_remaining:
-                raster_uri = join(train_img_dir, area, item.id, f"{item.id}_{i}.tif")
-                if file_exists(raster_uri):
-                    raster_source = (
-                        rv.RasterSourceConfig.builder(rv.RASTERIO_SOURCE)
-                        .with_uri(raster_uri)
-                        .with_channel_order([0, 1, 2])
-                        .build()
-                    )
-                    label_raster_source = (
-                        rv.RasterSourceConfig.builder(rv.RASTERIZED_SOURCE)
-                        .with_vector_source(label_uri)
-                        .with_rasterizer_options(2)
-                        .build()
-                    )
-                    label_source = (
-                        rv.LabelSourceConfig.builder(rv.SEMANTIC_SEGMENTATION)
-                        .with_raster_source(label_raster_source)
-                        .build()
-                    )
-                    scene = (
-                        rv.SceneConfig.builder()
-                        .with_task(task)
-                        .with_id(f"{item.id}_{i}")
-                        .with_raster_source(raster_source)
-                        .with_label_source(label_source)
-                        .build()
-                    )
-                    scenes.append(scene)
-                else:
-                    images_remaining = False
-                i += 1
-
-            return scenes
-
-        def make_test_scene(item):
-            raster_uri = join(test_img_dir, item.id, f"{item.id}.tif")
-            raster_source = (
-                rv.RasterSourceConfig.builder(rv.RASTERIO_SOURCE)
-                .with_uri(raster_uri)
-                .with_channel_order([0, 1, 2])
-                .build()
-            )
-            scene = (
-                rv.SceneConfig.builder()
-                .with_id(item.id)
-                .with_raster_source(raster_source)
-                .build()
-            )
-
-            return scene
-
         classes = {"No Building": (2, "#ff00ff"), "Building": (1, "#e6194b")}
+
+        # Create train, validation and test scenes
+        print("Creating train scenes")
+        train_items = [train_stac.get_child(c).get_item(i) for c, i in train_ids]
+        train_scenes = [
+            make_train_scenes(item, train_stac_uri, train_img_dir)
+            for item in train_items
+        ]
+        train_scenes = [item for sublist in train_scenes for item in sublist]
+
+        print("Creating validation scenes")
+        valid_items = [train_stac.get_child(c).get_item(i) for c, i in valid_ids]
+        valid_scenes = [
+            make_train_scenes(item, train_stac_uri, train_img_dir)
+            for item in valid_items
+        ]
+        valid_scenes = [item for sublist in valid_scenes for item in sublist]
+
+        # Take random sample of validation scenes
+        if len(valid_scenes) > 30:
+            valid_scenes = sample(valid_scenes, 30)
+
+        print("Creating test scenes")
+        test_scenes = [make_test_scene(item, train_img_dir) for item in all_test_items]
+
+        if test:
+            train_scenes = train_scenes[:3]
+            valid_scenes = valid_scenes[:3]
+
         # Configure the semantic segmentation task
         task = (
             rv.TaskConfig.builder(rv.SEMANTIC_SEGMENTATION)
@@ -143,38 +104,9 @@ class Experiment(rv.ExperimentSet):
             rv.BackendConfig.builder(PYTORCH_SEMANTIC_SEGMENTATION)
             .with_task(task)
             .with_train_options(**config)
+            .with_pretrained_uri("s3://carderne-rv/train/benchmark1/model")
             .build()
         )
-
-        # Create train, validation and test scenes
-        print("Creating train scenes")
-        train_scenes = reduce(
-            lambda a, b: a + b,
-            [
-                make_train_scenes(train_stac.get_child(c).get_item(i))
-                for c, i in train_ids
-            ],
-        )
-
-        print("Creating validation scenes")
-        valid_scenes = reduce(
-            lambda a, b: a + b,
-            [
-                make_train_scenes(train_stac.get_child(c).get_item(i))
-                for c, i in valid_ids
-            ],
-        )
-        # Take random sample of validation scenes
-        if len(valid_scenes) > 30:
-            valid_scenes = sample(valid_scenes, 30)
-
-        print("Creating test scenes")
-        test_scenes = [make_test_scene(item) for item in all_test_items]
-
-        if test:
-            train_scenes = train_scenes[:3]
-            valid_scenes = valid_scenes[:3]
-
         # Create DataSet with train, validation and test scenes
         print("Building dataset config")
         dataset = (
@@ -213,3 +145,65 @@ class Experiment(rv.ExperimentSet):
         )
 
         return experiment
+
+
+def make_train_scenes(item, train_stac_uri, train_img_dir):
+    area = item.get_parent().id
+    label_uri = join(
+        dirname(train_stac_uri), area, f"{item.id}-labels", f"{item.id}.geojson"
+    )
+
+    i = 0
+    images_remaining = True
+    scenes = []
+    while images_remaining:
+        raster_uri = join(train_img_dir, area, item.id, f"{item.id}_{i}.tif")
+        if file_exists(raster_uri):
+            raster_source = (
+                rv.RasterSourceConfig.builder(rv.RASTERIO_SOURCE)
+                .with_uri(raster_uri)
+                .with_channel_order([0, 1, 2])
+                .build()
+            )
+            label_raster_source = (
+                rv.RasterSourceConfig.builder(rv.RASTERIZED_SOURCE)
+                .with_vector_source(label_uri)
+                .with_rasterizer_options(2)
+                .build()
+            )
+            label_source = (
+                rv.LabelSourceConfig.builder(rv.SEMANTIC_SEGMENTATION)
+                .with_raster_source(label_raster_source)
+                .build()
+            )
+            scene = (
+                rv.SceneConfig.builder()
+                .with_id(f"{item.id}_{i}")
+                .with_raster_source(raster_source)
+                .with_label_source(label_source)
+                .build()
+            )
+            scenes.append(scene)
+        else:
+            images_remaining = False
+        i += 1
+
+    return scenes
+
+
+def make_test_scene(item, test_img_dir):
+    raster_uri = join(test_img_dir, item.id, f"{item.id}.tif")
+    raster_source = (
+        rv.RasterSourceConfig.builder(rv.RASTERIO_SOURCE)
+        .with_uri(raster_uri)
+        .with_channel_order([0, 1, 2])
+        .build()
+    )
+    scene = (
+        rv.SceneConfig.builder()
+        .with_id(item.id)
+        .with_raster_source(raster_source)
+        .build()
+    )
+
+    return scene
